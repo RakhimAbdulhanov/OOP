df653e9f42c9520c42d7d586a0917f7d493dddec
RakhimAbdulhanov <AbdulRakhim.Abdulkhanov@lnu.edu.ua>
2025-04-23 10:49:59 +0300

    Update main.cpp

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -1,12 +1,14 @@
 #include "Dyhotomia_class.h"
 #include "Newton_class.h"
+#include "func.h"
 #include <iostream>
-
+#include <iomanip>  
+using namespace std;
 int main(){
     double a, b, eps;
     Dyhotomia_class * dyh = new Dyhotomia_class();
     Newton_class * newton = new Newton_class();
-    std::cin >> a >> b >> eps;
+    cin >> a >> b >> eps;
     dyh->setVolumes(a,b);
     dyh->setTolerance(eps);
     double x_dyh = dyh->solve();
@@ -14,9 +16,9 @@ int main(){
     newton->setVolumes(a,b);
     newton->setTolerance(eps);
     double x_newton = newton->solve();
-    std::cout << "\nroot(dyho): " << x_dyh << std::endl;
-    std::cout << "root(newton): " << x_newton << std::endl;
-    delete dyh;
-    delete newton;
+   
+    cout << "\nroot(dyho): " << fixed << setprecision(15) << x_dyh << endl;
+    cout << "root(newt): " << fixed << setprecision(15) << x_newton << endl;
+    cout << "f(x_new): " << f(x_newton) << " f(x_dyh): " << f(x_dyh);
     return 0;
 }
